302343bfd2fab5a11779bfcb3c9db45daf1f7ff3
Manuel de Jesus Veliz Najera <manueldejesusveliznajera@MacBook-Pro-de-Joksan.local>
2022-10-18 20:52:51 -0600

    se edito appserviceprovider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 
@@ -26,5 +27,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
